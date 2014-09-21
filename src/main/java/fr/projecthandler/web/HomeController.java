1f942e153ae6297ae0964af3a99638037dcf7c23
cyril <cf1@self-med.fr>
2014-09-21 17:02:28 +0100

    remove log in catalina.out

--- src/main/java/fr/projecthandler/web/HomeController.java
+++ src/main/java/fr/projecthandler/web/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		System.out.println("Welcome home!");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
