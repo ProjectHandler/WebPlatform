6fe3c8ae712daebe41ce555079c4f25b8d6c3d0d
cyril <cf1@self-med.fr>
2014-11-14 14:43:11 +0000

    update database for groups

--- src/main/resources/db/alter.sql
+++ src/main/resources/db/alter.sql
@@ -22,3 +22,18 @@ ALTER TABLE `users` ADD `civility` int(4) DEFAULT NULL;
 ALTER TABLE `users` CHANGE `first_name` `first_name` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 ALTER TABLE `users` CHANGE `last_name` `last_name` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 ALTER TABLE `users` CHANGE `password` `password` VARCHAR(70) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+
+
+-- Table structure for table `users_groups`
+-- 14/11/2014
+
+CREATE TABLE IF NOT EXISTS `users_groups` (
+  `users_id` bigint(20) NOT NULL,
+  `groups_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`users_id`,`groups_id`),
+  KEY `users_groups_ibfk_2` (`groups_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `users_groups`
+  ADD CONSTRAINT `users_groups_ibfk_2` FOREIGN KEY (`groups_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `users_groups_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
