037bc7e5da136d4910602112d9b902e34a556a24
fillat-c <>
2015-11-14 21:59:01 +0100

    fix utf8 in bdd

--- src/main/resources/db/alter.sql
+++ src/main/resources/db/alter.sql
@@ -1,2 +1,9 @@
 -- Add draft message (guilla_e) (22/10/2015)
 ALTER TABLE `users` ADD `draft_message` varchar(500) CHARACTER SET utf8 DEFAULT NULL;
+
+-- use utf8_general_ci (14/11/2015)
+ALTER TABLE `groups` CHANGE `name` `name` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `project` CHANGE `status` `status` VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+ALTER TABLE `users` CHANGE `email` `email` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, 
+	CHANGE `phone` `phone` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL, 
+	CHANGE `mobile_phone` `mobile_phone` VARCHAR(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
